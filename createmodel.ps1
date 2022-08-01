1f4efa5d2cf4a2ebebefff571f6a82666ec882be
Matheus Gomes <103961000+MatheusGomes2003@users.noreply.github.com>
2022-08-01 16:08:21 -0300

    model

--- createmodel.ps1
+++ createmodel.ps1
@@ -3,3 +3,4 @@ dotnet add package Microsoft.EntityFrameworkCore.Design
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 dotnet tool install --global dotnet-ef
 dotnet ef dbcontext scaffold $strconn Microsoft.EntityFrameworkCore.SqlServer --force -o Model
+Set-ExecutionPolicy- Scope Process -ExecutionPolicy ByPass
