34df178770e4c04212db26288e9a6d5363b16f05
Matheus Gomes <103961000+MatheusGomes2003@users.noreply.github.com>
2022-08-01 17:23:57 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProjectSenai.Model;
+
+
+while(true)
+{
+ ExemploSenaiContext context = new ExemploSenaiContext();
+ Console.WriteLine("O que deseja fazer? 1- login, 2- Registro ou 3 - Sair");
+ string num = Console.ReadLine();
+ if (num == "1")
+ {
+    foreach(Usuario usuario in context.Usuarios)
+    {
+        Console.Clear();
+    }
+ }
+ else if(num == "2")
+ {
+    Console.Clear();
+ }
+ else if(num == "3")
+{
+   Console.Clear();
+}
+else
+    Console.WriteLine("Operação mal sucecida");
+}
+
+
+
+
